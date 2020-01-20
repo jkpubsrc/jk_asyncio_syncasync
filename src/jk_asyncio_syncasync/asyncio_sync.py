95817f3f6055bbcffdf6bc7a0f9e25a53ad9237b
jkpubsrc <pubsrc@binary-overflow.de>
2020-01-20 19:40:32 +0100

    Nothing changed.

--- src/jk_asyncio_syncasync/asyncio_sync.py
+++ src/jk_asyncio_syncasync/asyncio_sync.py
@@ -12,13 +12,13 @@ import asyncio
 
 
 executor = concurrent.futures.ThreadPoolExecutor(max_workers=32)
-loop = asyncio.get_event_loop()
 
 
 
 async def call_sync(synchroneousCallable, *args, **kwargs):
 	assert callable(synchroneousCallable)
 
+	loop = asyncio.get_event_loop()
 	result = await loop.run_in_executor(executor, synchroneousCallable, *args, **kwargs)
 	return result
 #
@@ -31,6 +31,7 @@ def make_async(synchroneousCallable):
 	assert callable(synchroneousCallable)
 
 	async def func_wrapper(*args, **kwargs):
+		loop = asyncio.get_event_loop()
 		result = await loop.run_in_executor(executor, synchroneousCallable, *args, **kwargs)
 		return result
 	#
